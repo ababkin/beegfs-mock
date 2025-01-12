f5256c02a4d9093dfd90afe68c00127a000bdfec
Alex Babkin <ababkin@Alexs-MacBook-Pro.local>
2025-01-12 15:14:50 +0100

    proper output

--- src/Mock.hs
+++ src/Mock.hs
@@ -4,8 +4,11 @@
 module Mock (runMock) where
 
 import Options.Applicative (execParser)
-import Data.Aeson (Value)
+import Data.Aeson (Value(..), encode, Object)
+import qualified Data.Aeson.KeyMap as KM
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Text as T
 
 import BeeGfsOptions
     ( BeeGfsCommand(..)
@@ -34,17 +37,20 @@ handleGetQuota GetQuotaOpts{..} = do
         gqSelection
     case result of
         Left err -> putStrLn $ "Error: " ++ show err
-        Right response -> putStrLn $ "Response: " ++ show response
+        Right (Object obj) -> case KM.lookup "csv" obj of
+            Just (String csv) -> putStr $ T.unpack csv  -- For CSV responses
+            _ -> BL.putStrLn $ encode obj          -- For JSON responses
+        Right val -> BL.putStrLn $ encode val      -- For other JSON values
 
 -- | Handle the set-quota command
 handleSetQuota :: SetQuotaOpts -> IO ()
 handleSetQuota SetQuotaOpts{..} = do
     result <- Api.setQuota 
-        sqGid            -- String
-        sqSizeLimit      -- String
-        sqInodeLimit     -- String
-        sqMount          -- FilePath
-        sqUnlimitedInodes -- Bool
+        sqGid
+        sqSizeLimit
+        sqInodeLimit
+        sqMount
+        sqUnlimitedInodes
     case result of
         Left err -> putStrLn $ "Error: " ++ show err
-        Right response -> putStrLn $ "Response: " ++ show response
+        Right val -> BL.putStrLn $ encode val
